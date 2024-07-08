8083479e9bba7a4e5721aae790135f5d63b81916
gombedlm <114173194+gombedlm@users.noreply.github.com>
2024-07-08 13:02:03 -0400

    updated app.py

--- main/app.py
+++ main/app.py
@@ -1,19 +1,34 @@
 # app.py
 
 from flask import Flask, render_template
-app = Flask(__name__)
 
-# Route for the dashboard
+app = Flask(__name__, static_url_path='/static', static_folder='static')
+
+# Index route
 @app.route('/')
-def dashboard():
-    print("Welcome to HomeGuard IDS Dashboard")
+def index():
     return render_template('dashboard.html')
 
+# Settings route
+@app.route('/settings')
+def settings():
+    return render_template('settings.html')
 
-# Route for the about page
+# Dashboard route
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+# Monitoring route
+@app.route('/monitoring')
+def monitoring():
+    return render_template('monitoring.html')
+
+# About route
 @app.route('/about')
 def about():
     return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
+
