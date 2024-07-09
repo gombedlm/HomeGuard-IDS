3fedb4164ebae3f47d81da916552674c34e63f3d
gombedlm <114173194+gombedlm@users.noreply.github.com>
2024-07-09 06:53:43 -0400

    created logging.py

---
+++ main/src/logging/logging.py
@@ -0,0 +1,17 @@
+import logging
+
+class Logger:
+    def __init__(self):
+        logging.basicConfig(filename='app.log', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+
+    def info(self, message):
+        logging.info(message)
+
+    def warning(self, message):
+        logging.warning(message)
+
+    def error(self, message):
+        logging.error(message)
+
+    def critical(self, message):
+        logging.critical(message)
